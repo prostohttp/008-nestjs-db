8539f2f3a1a8aafd10de81cd8a8e9be29f284b8e
Morozov Mikhail <memphis.sail@gmail.com>
2023-11-03 17:05:12 +1000

    - dotenv + @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
-import "dotenv/config";
+import { ConfigModule } from "@nestjs/config";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { BooksModule } from "./books/books.module";
 
 @Module({
-  imports: [BooksModule, MongooseModule.forRoot(process.env.MONGO_URL)],
+  imports: [
+    BooksModule,
+    ConfigModule.forRoot({}),
+    MongooseModule.forRoot(process.env.MONGO_URL),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
